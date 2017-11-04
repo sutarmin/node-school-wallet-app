6fc870fff189d48e7ce572c128c53cfabd2aacd2
Alexey Vykhrystyuk <AVykhrystyuk@gmail.com>
2017-11-05 00:37:55 +0300

    added removing of obsolete cache

--- source/client/service-worker/sw.js
+++ source/client/service-worker/sw.js
@@ -1,12 +1,14 @@
+/* eslint-disable */
 importScripts('/sw-cache-polyfill.js');
 
 const cacheVersion = 1;
 const currentCache = {
+	versionWhitelist: [],
 	name: `offline v${cacheVersion}`
 };
 // const offlineUrl = 'offline-page.html';
 
-this.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
 	// event.waitUntil(caches.open(currentCache.offline).then((cache) => {
 	// 	return cache.addAll([
 	// 		'./img/offline.svg',
@@ -14,3 +16,32 @@ this.addEventListener('install', (event) => {
 	// 	]);
 	// }));
 });
+
+self.addEventListener('activate', event => {
+	event.waitUntil(
+		deleteObsoleteCache()
+	);
+});
+
+self.addEventListener('fetch', event => {
+	console.log(event.request.url);
+});
+
+function deleteObsoleteCache() {
+	return caches.keys().then(keys => {
+		const keysToDelete = keys.filter(key => {
+			if (key === currentCache.name) {
+				return false;
+			}
+
+			if (currentCache.versionWhitelist.indexOf(key) !== -1) {
+				return false;
+			}
+
+			return true;
+		})
+
+		const deletePromises = keysToDelete.map(key => caches.delete(key));
+		return Promise.all(deletePromises);
+	});
+}
